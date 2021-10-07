a2f2578ec0bc25f4c830bd31f1c444054740957d
Coloursplash <darealredshift@gmail.com>
2021-10-07 14:32:07 +0100

    Finished

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# TicTac-2.0
- 
+# Tic Tac 2.0
+The childhood game of Tic Tac Toe except you can edit the grid..
+
+## Rules
+X player goes first, then O and they alternate turns.
+You win if you get three of your character in a row, column or diagonal.
+
+By default, the grid starts with 4 empty tiles vertically in the center. The rest of the grid isn't valid play space.
+
+Each turn 1 of 3 actions can be made:
+1. Place you symbol on an empty grid tile
+2. Create 4 new grid tiles
+3. Block two empty grid tiles
+
+1. Your symbol has to be on an empty grid tile, you can't replace an opponents or place it on a non-existing tile (shown with a '|')
+2. The first new tile must neighbour an already existing tile. All 4 newly created grid tiles must be neighbouring in any direction. By neighbouring, it refers to the diagram below.
+❌❌❌❌❌
+❌✓ ✓ ✓ ❌
+❌✓ T ✓ ❌
+❌✓ ✓ ✓ ❌
+❌❌❌❌❌
+3. Like action 1, only empty grid tiles can be blocked. A blocked tile cannot be replaced with a symbol.
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ SIZE = 11
 grid = []
 
 # create grid
-for i in range(SIZE):
+for y in range(SIZE):
     grid.append([])
-    for j in range(SIZE):
-        grid[i].append("-")
+    for x in range(SIZE):
+        grid[y].append("-")
 
 # add starting area
 middleX = math.trunc(SIZE / 2)
@@ -19,7 +19,8 @@ for i in range(-2, 2):
     grid[middleY + i][middleX] = "|"
 
 currentPlayer = "X"
-gameNotOver = True
+gameOver = False
+reason = ""
 
 # clears the screen
 def clear():
@@ -42,14 +43,14 @@ def changePlayer():
 def printGrid():
     # print the x axis at the top
     for i in range(SIZE + 1):
-        print(i[-1], end = " ")
+        print(str(i)[-1], end = " ")
     
     # create new line
     print()
 
     yGuide = 1
     for line in grid:
-        print(yGuide[-1], end = " ")
+        print(str(yGuide)[-1], end = " ")
         for location in line:
             print(location, end = " ")
         yGuide += 1
@@ -57,27 +58,37 @@ def printGrid():
         # create new line
         print()
 
-# print starting info
-print("RULES WILL GO HERE")
 printGrid()
 
 # main game loop
-while gameNotOver:    
+while not gameOver:
     # get action type
     while True:
-        action = input("Choose your action:\n1. Place a symbol\n2. Create 4 new connecting squares\n3. Delete any two unfilled squares\n")
+        print("Player " + currentPlayer)
+        action = input("Choose your action:\n1. Place a symbol\n2. Create 4 new connecting tiles\n3. Block any two unfilled tiles\n")
         if action.isdigit():
-            if action not in [1, 2, 3]:
-                print("Please enter 1, 2 or 3")
+            action = int(action)
+            if action in [1, 2, 3]:
                 break
+            else:
+                print("Please enter 1, 2 or 3")
         else:
             print("Please enter 1, 2 or 3")
     
     # if place a symbol is chosen
     # get x and y
     # both must be within the bounds and the selected square must be a | meaning it is empty
-    if action == "1":
+    if action == 1:
         while True:
+            # check if there are any empty tiles
+            possibleLocationCheck = False
+            for line in grid:
+                if "|" in line:
+                    possibleLocationCheck = True
+
+            if not possibleLocationCheck:
+                break
+
             # get x
             while True:
                 coordinate = input("Enter x coordinate: ")
@@ -105,13 +116,186 @@ while gameNotOver:
                     print("Please enter a number")
             
             if grid[y][x] != "|":
-                print("The selected tile isn't an empty square")
+                print("The selected tile isn't an empty square (|)")
             else:
                 grid[y][x] = currentPlayer
                 break
-    
+        
+        # 3 in a row check
+        # horizontal check
+        for y in range(SIZE):
+            for x in range(SIZE - 2):
+                if (grid[y][x] == currentPlayer) and (grid[y][x+1] == currentPlayer) and (grid[y][x+2] == currentPlayer):
+                    gameOver = True
+        # vertical check
+        for y in range(SIZE - 2):
+            for x in range(SIZE):
+                if (grid[y][x] == currentPlayer) and (grid[y+1][x] == currentPlayer) and (grid[y+2][x] == currentPlayer):
+                    gameOver = True
+        # top left to bottom right check
+        for y in range(SIZE - 2):
+            for x in range(SIZE - 2):
+                if (grid[y][x] == currentPlayer) and (grid[y+1][x+1] == currentPlayer) and (grid[y+2][x] == currentPlayer):
+                    gameOver = True
+        # bottom left to top right check
+        for y in range(2, SIZE):
+            for x in range(SIZE - 2):
+                if (grid[y][x] == currentPlayer) and (grid[y-1][x+1] == currentPlayer) and (grid[y-2][x] == currentPlayer):
+                    gameOver = True
+        if gameOver:
+            reason = currentPlayer + " won!"
+    # first tile must neighbour a | X O #
+    # al the rest must neigherbour a T
+    elif action == 2:
+        isFirst = True
+        # loop this four times as we want to add 4 squares
+        for i in range(4):
+            # check if the grid is full
+            possibleLocationCheck = False
+            for line in grid:
+                if "-" in line:
+                    possibleLocationCheck = True
+
+            if not possibleLocationCheck:
+                break
+
+            while True:
+                # get x
+                while True:
+                    coordinate = input("Enter x coordinate: ")
+                    if coordinate.isdigit():
+                        if 0 < int(coordinate) <= SIZE:
+                            coordinate = int(coordinate) - 1
+                            x = coordinate
+                            break
+                        else:
+                            print("Please enter a number between 1 and " + str(SIZE))
+                    else:
+                        print("Please enter a number")
+                
+                # get y
+                while True:
+                    coordinate = input("Enter y coordinate: ")
+                    if coordinate.isdigit():
+                        if 0 < int(coordinate) <= SIZE:
+                            coordinate = int(coordinate) - 1
+                            y = coordinate
+                            break
+                        else:
+                            print("Please enter a number between 1 and " + str(SIZE))
+                    else:
+                        print("Please enter a number")
+                
+                # check neighbours
+                # isFirst does same check but looks for | X O rather than T
+                if isFirst:
+                    validTiles = ["|", "X", "O", "#"]
+                else:
+                    validTiles.append("T")
+
+                valid = False
+                if grid[y][x] == "-":
+                    if (grid[y+1][x] in validTiles) and (y+1 <= SIZE):
+                        valid = True
+                    elif (grid[y-1][x] in validTiles) and (y-1 >= 0):
+                        valid = True
+                    elif (grid[y][x+1] in validTiles) and (x+1 <= SIZE):
+                        valid = True
+                    elif (grid[y][x-1] in validTiles) and (x-1 >= 0):
+                        valid = True
+                    elif (grid[y+1][x+1] in validTiles) and (y+1 <= SIZE) and (x+1 <= SIZE):
+                        valid = True
+                    elif (grid[y-1][x+1] in validTiles) and (y-1 >= 0) and(x+1 <= SIZE):
+                        valid = True
+                    elif (grid[y+1][x-1] in validTiles) and (y+1 <= SIZE) and(x-1 >= 0):
+                        valid = True
+                    elif (grid[y-1][x-1] in validTiles) and (y-1 >= 0) and (x-1 >= 0):
+                        valid = True
+                    
+                    if valid:
+                        grid[y][x] = "T"
+                        if isFirst:
+                            isFirst = False
+                        break
+                    else:
+                        if isFirst:
+                            print("The selected tile doesn't neighbour an existing tile (| O X #)")
+                        else:
+                            print("The selected tile doesn't neighbour a newly created tile (T)")
+                else:
+                    print("The selected tile already exists")
+            # after placing the T, reprint the grid so the user can see where to go next
+            print()
+            printGrid()
+        
+        # once all 4 are selected, change all Ts to |
+        for y in range(SIZE):
+            for x in range(SIZE):
+                if grid[y][x] == "T":
+                    grid[y][x] = "|"
+    # replace - with #
+    elif action == 3:
+        # repeat twice
+        for i in range(2):
+            # check if there are any empty tiles
+            possibleLocationCheck = False
+            for line in grid:
+                if "|" in line:
+                    possibleLocationCheck = True
+
+            if not possibleLocationCheck:
+                break
+
+            while True:
+                # get x
+                while True:
+                    coordinate = input("Enter x coordinate: ")
+                    if coordinate.isdigit():
+                        if 0 < int(coordinate) <= SIZE:
+                            coordinate = int(coordinate) - 1
+                            x = coordinate
+                            break
+                        else:
+                            print("Please enter a number between 1 and " + str(SIZE))
+                    else:
+                        print("Please enter a number")
+                
+                # get y
+                while True:
+                    coordinate = input("Enter y coordinate: ")
+                    if coordinate.isdigit():
+                        if 0 < int(coordinate) <= SIZE:
+                            coordinate = int(coordinate) - 1
+                            y = coordinate
+                            break
+                        else:
+                            print("Please enter a number between 1 and " + str(SIZE))
+                    else:
+                        print("Please enter a number")
+                
+                if grid[y][x] != "|":
+                    print("The selected tile isn't an empty square (|)")
+                else:
+                    grid[y][x] = "#"
+                    break
+            # after placing the #, reprint the grid so the user can see where to go next
+            print()
+            printGrid()
+
+    # check if grid is full
+    actionPossible = False
+    for line in grid:
+        if ("|" in line) or ("-" in line):
+            actionPossible = True
+    if not actionPossible:
+        gameOver = True
+        reason = "No possible moves, DRAW!"
+
     # reprint the grid
     clear()
     printGrid()
 
     changePlayer()
+
+print()
+print(reason)
